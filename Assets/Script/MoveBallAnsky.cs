33feb2cf8cef8385488ffe437e245a65c13b6d43
angskyee <149351121+angskyee@users.noreply.github.com>
2023-12-01 17:05:47 +0900

    Ball move and reflect
    공 움직임 과 반사 랑 벽

---
+++ Assets/Script/MoveBallAnsky.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    [SerializeField] float speed = 100f;
+    public Rigidbody2D rb;
+    float x, y;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        x = 0;
+        y = 1;
+        Vector2 dir = new Vector2(x, y).normalized;
+
+        rb.AddForce(dir * speed);
+    }
+}
