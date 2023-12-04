43fe4602899b8405b01f9fdedb20ff6362a35a00
colarrot <gunho7935@gmail.com>
2023-12-04 21:26:05 +0900

    게임타이틀,시작,스킨선택 씬

---
+++ Assets/Script/Button/SelectScene/ChooseDOG.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHooseDOg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DOGImage()
+    {
+        // 에셋에서 강아지 이미지를 패들로 선택
+    }
+}
